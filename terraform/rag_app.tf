f8b4edc2aa9fe3e618ce616621a390b1fa90f589
yoshitaka <somebody@somebodys-MacBook-Air.local>
2025-08-07 02:22:32 +0900

    fix: add ) and fix the bug of ra_app.tf

--- terraform/rag_app.tf
+++ terraform/rag_app.tf
@@ -4,19 +4,21 @@
 resource "google_cloud_run_v2_service" "rag_app" {
   name     = "rag-portfolio-app-${var.environment}"
   location = var.region
-  deletion_protection = false # この設定を確認・追加
 
   template {
     service_account = google_service_account.rag_app_sa.email
 
     containers {
-      image = "asia-northeast1-docker.pkg.dev/${var.project_id}/my-app-repo/rag-app:v1" # 手動ビルドしたイメージを指定
+      # このイメージ名は手動でビルドしたものに合わせます
+      image = "asia-northeast1-docker.pkg.dev/${var.project_id}/my-app-repo/rag-app:v1"
       ports {
         container_port = 8080
       }
+      
       env {
         name  = "VECTOR_BUCKET_NAME"
         value = google_storage_bucket.output.name
+      }
     }
   }
 
